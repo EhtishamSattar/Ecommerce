c6f2f15f4177a47c4392eda7f83606902e0be48f
Ehtisham <ehtishamsattar888@gmail.com>
2023-01-06 00:35:26 +0500

    footer added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Items from './Components/Items';
 import Cart from './Components/Cart';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div>
       <Cart />
-      <div className='mx-10 mb-10'>
+      <div className='mx-10 mb-3'>
         <Items />
       </div>
+      <Footer/>
     </div>
 
   );
--- src/Components/Items.js
+++ src/Components/Items.js
@@ -18,8 +18,8 @@ const Items = () => {
 
 
   return (
-    <div>
-      <div className="bg-white">
+    <>
+      <div className="bg-white ">
         <div className="mx-auto max-w-2xl px-4 sm:py-24 sm:px-6 lg:max-w-5xl lg:px-8">
           <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
             {things.map((e) => {
@@ -32,8 +32,7 @@ const Items = () => {
           </div>
         </div>
       </div>
-
-    </div>
+    </>
   )
 }
 
